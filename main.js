bd3b6f7f2b8df4aa921a2015234be53b160783e9
israel sofer <ysrlswpr00@gmail.com>
2023-08-02 17:32:08 +0300

    “srulik-project”

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 "use strict";
-let selectedCoin = [];
 $(() => {
+  let selectedCoin = [];
   userImage();
   const currenciesLink = document.getElementById("currenciesLink");
   const reportsLink = document.getElementById("reportsLink");
   const aboutLink = document.getElementById("aboutLink");
-  const mainContant = document.getElementById("mainContant")
+  const mainContent = document.getElementById("mainContent")
 
   currenciesLink.addEventListener("click", displayCurrencies);
   reportsLink.addEventListener("click", displayReports);
   aboutLink.addEventListener("click", displayAbout);
 
-  function displayCurrencies(){
-    mainContant.innerHTML = `<h1>Currencies...</h1>`
+  function displayCurrencies() {
+    mainContent.innerHTML = `<h1>Currencies...</h1>`
   }
 
-  function displayReports(){
-    mainContant.innerHTML = `<h1>Repoets...</h1>`
+  function displayReports() {
+    mainContent.innerHTML = `<h1>Reports...</h1>`
   }
 
-  function displayAbout(){
-    mainContant.innerHTML = `<h1>About...</h1>`
+  function displayAbout() {
+    mainContent.innerHTML = `<h1>About...</h1>`
   }
 
   const searchBar = document.getElementById("searchBar");
@@ -56,86 +56,64 @@ $(() => {
       $("#mainContent").append(`
         <div id = "${photos[i].id}" class="card" style="width: 18rem;">
         <div id = "Toggle" class="form-switch">
-        <input class="form-check-input" type="checkbox" role="switch" value="${photos[i].symbol}">
+        <input class="form-check-input" type="checkbox" role="switch" value="${photos[i].id}">
       </div>
           <img src="${photos[i].image}" class="card-coins">
           <div class="card-body">
             <h5 class="card-title">${photos[i].symbol}</h5>
             <a href="" class="btn btn-primary">more info</a>
+            <div class="spinner-container">
+  <div class="spinner"></div>
+</div>
           </div>
            <div class="moreInfo">
-           <div name="spinner" class="spinner-border spinner-border-sm" role="status" style="display: none";></div>
-           </button>
-           <div>
-             <div class="collapse" id="collapseExample${i}">
-               <div id="infoCoin${i}" class="card card-body collapse-body"></div>
-             </div>
-           </div>
-         </div>
            </div>
         </div>
       `);
     }
-    $(".toggleCheck").on("click", function () {
-      if (this.checked === true) {
-        selectedCards.push(this.value);
-      }
-      if (this.checked === false) {
-        let uncheck = this.value;
-        const indexUncheck = selectedCards.findIndex((name) => name === uncheck);
-        selectedCards.splice(indexUncheck, 1);
-      }
-      if (selectedCards.length > 5) {
-        myModal.show();
-      }
-      const inModal = document.getElementById("inModal");
-      let htmlCard = "";
-      for (let i = 0; i < photos.length - 1; i++) {
-        htmlCard += `
-          <div class="card">
-              <div class="card-body cardModal">
-                <span class="nameSelectCard">${photos[i]}</span>
-                <span class="btn-toggle selected-toggle">
-                <input class="toggle-one ee" data-bs-dismiss="modal" type="checkbox" name="modalCheck" value="${selectedCards[i]}" id="checkModal${i}">
-                <label class="toggle" for="checkModal${i}"></label>
-              </span>
-              </div>
-            </div>`;
-      }
-      inModal.innerHTML = htmlCard;
+    // $(".toggleCheck").on("click", function () {
+    //   if (this.checked === true) {
+    //     selectedCards.push(this.value);
+    //   }
+    //   if (this.checked === false) {
+    //     let uncheck = this.value;
+    //     const indexUncheck = selectedCards.findIndex((name) => name === uncheck);
+    //     selectedCards.splice(indexUncheck, 1);
+    //   }
+    //   if (selectedCards.length > 5) {
+    //     myModal.show();
+    //   }
 
-      const buttonsArr = document.getElementsByClassName("moreInfo");
-    for (let i = 0; i < buttonsArr.length; i++) {
-      buttonsArr[i].addEventListener("click", async function () {
-        if (this.getAttribute("aria-expanded") == "true") {
-          const moreInfoCoin = await getMoreInfoFromSessionOrHttps(this);
-          const collapse = document.getElementById(`infoCoin${i}`);
-          let htmlCol = "";
-          for (const item of moreInfoCoin) {
-            if (item.name === this.id) {
-              htmlCol += `
-              USD : ${item.usd} $<br>
-              EUR : ${item.eur} €<br>
-              ILS : ${item.ils} ₪<br>`;
-              collapse.innerHTML = htmlCol;
-            }
-          }
-        }
-      });
-    }
-  })
+    //     const inModal = document.getElementById("inModal");
+    //     let htmlCard = "";
+    //     for (let i = 0; i < photos.length - 1; i++) {
+    //       htmlCard += `
+    //         <div class="card">
+    //             <div class="card-body cardModal">
+    //               <span class="nameSelectCard">${photos[i]}</span>
+    //               <span class="btn-toggle selected-toggle">
+    //               <input class="toggle-one ee" data-bs-dismiss="modal" type="checkbox" name="modalCheck" value="${selectedCards[i]}" id="checkModal${i}">
+    //               <label class="toggle" for="checkModal${i}"></label>
+    //             </span>
+    //             </div>
+    //           </div>`;
+    //     }
+    //     inModal.innerHTML = htmlCard;
+    //   })
+    //   }
+    // })
 
-  const modalClose = document.getElementById('modalClose');
-  modalClose.addEventListener('click', () => {
-    const lastItem = selectedCards[selectedCards.length - 1];
-    for (const x of checkboxes) {
-      if (x.value === lastItem) {
-        x.checked = false;
-      }
-    }
-    const index = selectedCards.findIndex(x => x === lastItem);
-    selectedCards.splice(index, 1);
-  });
+    // const modalClose = document.getElementById('modalClose');
+    // modalClose.addEventListener('click', () => {
+    //   const lastItem = selectedCards[photos.length - 1];
+    //   for (const x of checkboxes) {
+    //     if (x.value === lastItem) {
+    //       x.checked = false;
+    //     }
+    //   }
+    //   const index = selectedCards.findIndex(x => x === lastItem);
+    //   selectedCards.splice(index, 1);
+    // });
 
 
     const btns = document.querySelectorAll('.btn-primary');
@@ -143,24 +121,31 @@ $(() => {
       btn.addEventListener("click", async function (event) {
         event.preventDefault();
         const moreInfo = this.closest('.card').querySelector('.moreInfo');
-        moreInfo.style.display = (moreInfo.style.display === 'none') ? 'block' : 'none';
-        const cryptoId = this.closest(`.card`).id;
+        const spinnerContainer = this.closest('.card').querySelector('.spinner-container');
 
+        // הצגת הספינר
+        spinnerContainer.style.display = "flex";
+
+        const cryptoId = this.closest(`.card`).id;
         const json = await getCrypto(cryptoId);
+
+        // הסתרת הספינר לאחר שקיבלנו את המידע
+        spinnerContainer.style.display = "none";
+
+        moreInfo.style.display = (moreInfo.style.display === 'none') ? 'block' : 'none';
         moreInfo.innerHTML = `
-        <p class="card-text">price in USD: ${json.market_data.current_price.usd}$</p>
-        <p class="card-text">price in EURO: ${json.market_data.current_price.eur}€</p>
-        <p class="card-text">price in ILS: ${json.market_data.current_price.ils}₪</p>
-      `
-      })
-    
+          <p class="card-text">price in USD: ${json.market_data.current_price.usd}$</p>
+          <p class="card-text">price in EURO: ${json.market_data.current_price.eur}€</p>
+          <p class="card-text">price in ILS: ${json.market_data.current_price.ils}₪</p>
+        `
+      });
+
       async function getCrypto(id) {
         const data = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
         const json = await data.json();
         return json;
-      
       }
-    })
-
+    });
   }
 })
+
