9d8595b3b8a0b89e9897c9e0eeda99d14f8cf9fd
israel sofer <ysrlswpr00@gmail.com>
2023-08-12 15:20:19 +0300

    “srulik-project”

--- main.js
+++ main.js
@@ -2,32 +2,52 @@
 $(() => {
   userImage();
 
+  function saveToSessionStorage(key, moreInfo) {
+    sessionStorage.setItem(key, JSON.stringify(moreInfo));
+  }
+  
+
+  function getFromSessionStorage(key) {
+    const moreInfo = sessionStorage.getItem(key);
+    return value ? JSON.parse(moreInfo) : null;
+  }
+
+  
+
+
   const currenciesLink = document.getElementById("currenciesLink");
   const reportsLink = document.getElementById("reportsLink");
   const aboutLink = document.getElementById("aboutLink");
   const mainContent = document.getElementById("mainContent");
+  const container = document.getElementById("container");
 
   currenciesLink.addEventListener("click", displayCurrencies);
   reportsLink.addEventListener("click", displayReports);
   aboutLink.addEventListener("click", displayAbout);
 
   async function displayCurrencies() {
+    mainContent.innerHTML = '';
     const photos = await getJson();
     displayCrypto(photos);
+    // container.innerHTML = "";
   }
 
   function displayReports() {
+    mainContent.innerHTML = '';
     mainContent.innerHTML = `<h1>Reports...</h1>
     <img src="assets/working.webp" width="100%" height="100%">`
+    // container.innerHTML = "";
   }
 
   function displayAbout() {
+    mainContent.innerHTML = '';
     mainContent.innerHTML = `<h1>About...</h1>
     <img src="assets/israelSofer.jpg" width="200px">
     <p>A few words about myself: Hi, I'm Israel Sofer, I'm 20 years old, I work at Aroma (coffee shop) and I study at John Bryce full stack web 😁</p>
     <p>And a few words about the project I did: Decentralization: Unlike traditional currencies issued and controlled by governments or financial institutions, cryptocurrencies operate on decentralized networks using blockchain technology. This means no single entity has full control over the currency, enhancing security and reducing the risk of manipulation.
 
     Blockchain Technology: Cryptocurrencies rely on blockchain, a distributed ledger that records all transactions across a network of computers. Each block contains a group of transactions, and once added to the blockchain, it becomes immutable, creating a transparent and tamper-resistant record of all transactions.</p>`
+    // container.innerHTML = "";
   }
 
   const searchBar = document.getElementById("searchBar");
@@ -97,6 +117,7 @@ $(() => {
         await sleep(2000);
         const json = await getCrypto(cryptoId);
 
+        const id = this.closest(".card").id;
         spinnerContainer.style.display = "none";
 
         moreInfo.style.display = (moreInfo.style.display === 'none') ? 'block' : 'none';
@@ -105,6 +126,7 @@ $(() => {
           <p class="card-text">price in EURO: ${json.market_data.current_price.eur}€</p>
           <p class="card-text">price in ILS: ${json.market_data.current_price.ils}₪</p>
         `
+        saveToSessionStorage(id,moreInfo.innerHTML);
       });
 
       
