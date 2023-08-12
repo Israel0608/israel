0c8af3b80d83b90198ac0a42f9cae77fc23b1573
israel sofer <ysrlswpr00@gmail.com>
2023-08-12 15:41:43 +0300

    “srulik-project”

--- main.js
+++ main.js
@@ -5,21 +5,20 @@ $(() => {
   function saveToSessionStorage(key, moreInfo) {
     sessionStorage.setItem(key, JSON.stringify(moreInfo));
   }
-  
+
 
   function getFromSessionStorage(key) {
     const moreInfo = sessionStorage.getItem(key);
     return value ? JSON.parse(moreInfo) : null;
   }
 
-  
+
 
 
   const currenciesLink = document.getElementById("currenciesLink");
   const reportsLink = document.getElementById("reportsLink");
   const aboutLink = document.getElementById("aboutLink");
   const mainContent = document.getElementById("mainContent");
-  const container = document.getElementById("container");
 
   currenciesLink.addEventListener("click", displayCurrencies);
   reportsLink.addEventListener("click", displayReports);
@@ -29,25 +28,32 @@ $(() => {
     mainContent.innerHTML = '';
     const photos = await getJson();
     displayCrypto(photos);
-    // container.innerHTML = "";
   }
 
   function displayReports() {
     mainContent.innerHTML = '';
     mainContent.innerHTML = `<h1>Reports...</h1>
     <img src="assets/working.webp" width="100%" height="100%">`
-    // container.innerHTML = "";
   }
 
   function displayAbout() {
     mainContent.innerHTML = '';
     mainContent.innerHTML = `<h1>About...</h1>
     <img src="assets/israelSofer.jpg" width="200px">
-    <p>A few words about myself: Hi, I'm Israel Sofer, I'm 20 years old, I work at Aroma (coffee shop) and I study at John Bryce full stack web 😁</p>
-    <p>And a few words about the project I did: Decentralization: Unlike traditional currencies issued and controlled by governments or financial institutions, cryptocurrencies operate on decentralized networks using blockchain technology. This means no single entity has full control over the currency, enhancing security and reducing the risk of manipulation.
+    <p>A few words about myself: Hi, I'm Israel Sofer,
+     I'm 20 years old, I work at Aroma (coffee shop) 
+     and I study at John Bryce full stack web 😁</p>
 
-    Blockchain Technology: Cryptocurrencies rely on blockchain, a distributed ledger that records all transactions across a network of computers. Each block contains a group of transactions, and once added to the blockchain, it becomes immutable, creating a transparent and tamper-resistant record of all transactions.</p>`
-    // container.innerHTML = "";
+    <p>And a few words about the project I did: Decentralization: 
+    Unlike traditional currencies issued and controlled by governments or financial institutions, 
+    cryptocurrencies operate on decentralized networks using blockchain technology. This means no single entity has full control over the currency, 
+    enhancing security and reducing the risk of manipulation.
+
+    Blockchain Technology: Cryptocurrencies rely on blockchain, 
+    a distributed ledger that records all transactions across a network of computers.
+    Each block contains a group of transactions, and once added to the blockchain, 
+    it becomes immutable,
+     creating a transparent and tamper-resistant record of all transactions.</p>`
   }
 
   const searchBar = document.getElementById("searchBar");
@@ -102,15 +108,15 @@ $(() => {
     function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
-    
+
     const btns = document.querySelectorAll('.btn-primary');
     btns.forEach(function (btn) {
       btn.addEventListener("click", async function (event) {
         event.preventDefault();
         const moreInfo = this.closest('.card').querySelector('.moreInfo');
         const spinnerContainer = this.closest('.card').querySelector('.spinner-container');
-        
-       
+
+
         spinnerContainer.style.display = "flex";
 
         const cryptoId = this.closest(`.card`).id;
@@ -126,16 +132,45 @@ $(() => {
           <p class="card-text">price in EURO: ${json.market_data.current_price.eur}€</p>
           <p class="card-text">price in ILS: ${json.market_data.current_price.ils}₪</p>
         `
-        saveToSessionStorage(id,moreInfo.innerHTML);
+        saveToSessionStorage(id, moreInfo.innerHTML);
       });
 
-      
+
 
       async function getCrypto(id) {
         const data = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
         const json = await data.json();
         return json;
       }
+      let toggleButtonClickCount = 0;
+
+      function showModalIfNeeded() {
+        if (toggleButtonClickCount > 5) {
+          $('#staticBackdrop').modal('show');
+        }
+      }
+
+      const toggleButtons = document.querySelectorAll('.form-switch input[type="checkbox"]');
+      toggleButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          if (button.checked) {
+            toggleButtonClickCount++;
+            showModalIfNeeded();
+          } else {
+            toggleButtonClickCount--;
+          }
+        });
+      });
+
+     
+      const modalCloseButton = document.getElementById('modalClose');
+      modalCloseButton.addEventListener('click', () => {
+        toggleButtonClickCount--;
+      });
+      $(document).ready(function () {
+        showModalIfNeeded(); 
+      });
+
     });
   }
 })
